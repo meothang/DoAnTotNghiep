6e006852467b5ebfd041b9c7450a40411f339c55
Mai Linh <you@example.com>
2020-06-09 08:24:55 +0700

    update 69

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/backend';
 
     /**
      * Create a new controller instance.
