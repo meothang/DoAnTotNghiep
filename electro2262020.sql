2935aa0700074c164cd3e57dedd27da7e8703de9
meothang <meothang@yahoo.com.vn>
2020-06-23 15:22:28 +0700

    update

---
+++ electro2262020.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th6 22, 2020 lúc 08:12 AM
+-- Phiên bản máy phục vụ: 10.4.11-MariaDB
+-- Phiên bản PHP: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `electro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cate_slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cate_status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `cate_slug`, `cate_status`, `created_at`, `updated_at`) VALUES
+(1, 'Apple', 'apple', '1', '2020-04-10 07:03:13', '2020-04-10 07:03:13'),
+(10, 'Asus', 'asus', '1', '2020-06-15 05:20:33', '2020-06-15 05:20:33'),
+(11, 'HP', 'hp', '1', '2020-06-15 05:20:39', '2020-06-15 05:20:39'),
+(12, 'Lenovo', 'lenovo', '1', '2020-06-21 22:12:33', '2020-06-21 22:12:33');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `categories_name_unique` (`name`),
+  ADD KEY `categories_cate_slug_index` (`cate_slug`),
+  ADD KEY `categories_cate_status_index` (`cate_status`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ resources/views/backend/product.blade.php
@@ -0,0 +1,218 @@
+@extends('backend.layouts.backend-master')
+@section('backend-main')
+<!-- START BREADCRUMB -->
+<ul class="breadcrumb">
+    <li><a href="{{ route('admin.home')}}">Trang chủ</a></li>
+    <li><a href="{{ route('admin.get.list.product')}}">Sản phẩm</a></li>
+    <li class="active">Danh sách sản phẩm</li>
+</ul>
+<!-- END BREADCRUMB -->
+
+<!-- PAGE CONTENT WRAPPER -->
+<div class="page-content-wrap">
+    <!-- START RESPONSIVE TABLES -->
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="page-head-text col-md-4">
+                            <h1 class="panel-title"><strong>Quản lý</strong> sản phẩm</h1>
+                        </div>
+                        <div class="page-head-controls col-md-8">'.'
+                            <div class="row">
+                                <div class="col-md-9">
+                                    <form action="">
+                                        <div class="input-group" style="display: inline-flex;">
+                                            <span class="input-group-addon"><span class="fa fa-search"></span></span>
+                                            <input name="name" type="text" class="form-control"
+                                            placeholder="Nhập sản phẩm cần tìm kiếm"
+                                            value="{{ \Request::get('name') }}">
+                                            <div class="form-group" style="margin:0px">
+                                                <!-- <label for="inputState">Danh mục</label> -->
+                                                <select class="form-control select" name="cate" id="inputState">
+                                                    <!-- <option>Danh mục</option> -->
+                                                    @if($categories)
+                                                    @foreach($categories as $item)
+                                                    <option value="{{ $item->id }}"
+                                                        {{ \Request::get('cate') == $item->id ? "selected = 'selected'" : ""  }}>
+                                                        {{ $item->name }}
+                                                    </option>
+                                                    @endforeach
+                                                    @endif
+                                                </select>
+                                            </div>
+                                            <div class="input-group-btn">
+                                                <button type="submit" class="btn btn-primary">Search</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="col-md-3">
+                                    <a href="{{ route('admin.get.create.product')}}">
+                                        <button class="btn btn-primary btn-rounded" style="width:100%"><span
+                                            class="fa fa-plus"></span> Thêm
+                                        sản phẩm</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- <div class="row">
+                    <div class="col-md-6">
+                        <form action="" class="form-inline" style="margin-bottom :15px">
+                            <div class="form-group">
+                                <input class="form-control" type="text" name="name" placeholder="Search"
+                                    value="{{ \Request::get('name') }}">
+                            </div>
+                            <div class="form-group">
+                                <select name="cate" id=""
+                                    style="padding: 7px 0px;border-radius: 8%;border: 1px solid #ccc;">
+                                    <option value="">Danh mục </option>
+                                    @if($categories)
+                                    @foreach($categories as $item)
+                                    <option value="{{ $item->id }}"
+                                        {{ \Request::get('cate') == $item->id ? "selected = 'selected'" : ""  }}>
+                                        {{ $item->name }}
+                                    </option>
+                                    @endforeach
+                                    @endif
+                                </select>
+                            </div>
+                            <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                        </form>
+                    </div>
+                </div> --}}
+
+                <div class="panel-body panel-body-table">
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped table-actions">
+                            <thead>
+                                <tr>
+                                    <th width="50" class="text-center">ID</th>
+                                    <th width="250" class="text-center">Tên sản phẩm</th>
+                                    <th class="text-center">Mô tả</th>
+                                    <th width="150" class="text-center">Hình ảnh</th>
+                                    <th width="100" class="text-center">Loại</th>
+                                    <th width="120" class="text-center">Giá</th>
+                                    <th width="100" class="text-center">Danh mục</th>
+                                    <th width="50" class="text-center">Trạng thái</th>
+                                    <th width="120" class="text-center">Ngày nhập</th>
+                                    <th width="120" class="text-center">Hành động</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                @if(isset($products))
+                                @foreach($products as $product )
+                                <tr>
+                                    <td class="text-center">{{ $product->id }}</td>
+                                    <td><strong>{{ $product->pro_name}}</strong></td>
+                                    <td style="display: -webkit-box; -webkit-line-clamp: 4; overflow:
+                                    hidden; -webkit-box-orient: vertical;border-width:1px 0 0 0">
+                                    {{$product->pro_content}}</td>
+                                    <td class="text-center"><img class="img-fluid" style="width:100px"
+                                        {{-- src="{{ asset("img/product/$product->pro_image")}}" alt=""></td> --}}
+                                        src="{{asset("/img/product/".$product->categories -> name."/$product->pro_image")}}" alt=""></td>
+                                        <td class="text-center">{{ $product->product_type->name}}</td>
+                                        <td class="text-center">{{ number_format($product->pro_price) }} VNĐ</td>
+                                        <?php
+                                        $category=DB::table('categories')->where('id',$product->id)->first();
+                                        ?>
+                                        <td class="text-center">
+                                            {{ $product->getCategory() }}
+                                        </td>
+                                        <td class="text-center">
+                                            <a href="{{ route('admin.get.action.product',['status', $product -> id]) }}">
+                                                @if ($product -> status == 1)
+                                                <button type="button" class="btn btn-success">
+                                                    Hiển Thị
+                                                </button>
+                                                @else
+                                                <button type="button" class="btn btn-danger">
+                                                    Đã Ẩn
+                                                </button>
+                                                @endif
+
+                                            </a>
+                                        </td>
+                                        <td class="text-center">{{ date_format($product->created_at,'d/m/Y H:i:s') }}</td>
+                                        <td class="text-center">
+                                            <a>
+                                                <button class="btn btn-danger btn-rounded btn-condensed btn-sm notiDelete" data-id="{{$product -> id}}"><span
+                                                class="fa fa-times"></span></button>
+                                            </a>
+                                            <a href="{{ route('admin.get.edit.product',$product->id) }}">
+                                                <button class="btn btn-primary btn-rounded btn-condensed btn-sm">
+                                                    <span class="fa fa-pencil"></span></button>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                        @endif
+                                    </tbody>
+                                </table>
+
+                                <div class="message-box animated fadeIn" data-sound="alert" id="mb-remove-row">
+                                    <div class="mb-container">
+                                        <div class="mb-middle">
+                                            <div class="mb-title"><span class="fa fa-times"></span> Xác nhận
+                                                <strong>Xóa Sản Phẩm</strong> ?</div>
+                                                <div class="mb-content">
+                                                    <p>Nếu bạn muốn xóa sản phẩm này</p>
+                                                    <p>Hãy ấn XÓA</p>
+                                                </div>
+                                                <div class="mb-footer">
+                                                    <div class="pull-right">
+                                                         <button class="btn btn-warning btn-lg delPro">Xóa
+                                                        </button>
+                                                        <button class="btn btn-default btn-lg mb-control-close">Hủy</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- END MESSAGE BOX-->
+
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- END RESPONSIVE TABLES -->
+
+            </div>
+            @stop
+            @section('script')
+            <script>
+                $(".notiDelete").click(function(){
+                    $("#mb-remove-row").addClass("open");
+                    let id = $(this).data('id');
+                    $('.delPro').click(function(){
+                     $.ajax({
+                        url: 'backend/product/delete/'+id,
+                        data:{
+                            id: id,
+
+                        },
+                        dataType: 'json',
+                        type:'get',
+                        success:function($result){ 
+                          if ($result.success) {
+                            toastr.success($result.success, 'Thông Báo',{timeOut: 3000});
+                            location.reload();
+                        }else {
+                           toastr.error($result.error, 'Thông Báo',{timeOut: 3000});
+                     // location.reload();
+                 }
+             }
+
+         });
+
+                 });
+                });
+            </script>
+            @stop
